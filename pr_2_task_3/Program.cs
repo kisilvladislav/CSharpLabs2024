730f22653e4739a401c985beb5469c176c9926ff
WIN-PPAPV0NV3K2\Administrator <walfo2018@gmail.com>
2024-04-14 15:38:28 +0300

    3done

---
+++ pr_2_task_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int studentNumber = 13;
+        int arrayLength = 10 + (studentNumber % 10);
+
+        int[] X = new int[arrayLength];
+        Random rnd = new Random();
+        for (int i = 0; i < X.Length; i++)
+        {
+            X[i] = rnd.Next(100);
+        }
+
+        int min = X[0];
+        int max = X[0];
+
+        for (int i = 1; i < X.Length; i++)
+        {
+            if (X[i] < min)
+            {
+                min = X[i];
+            }
+            else if (X[i] > max)
+            {
+                max = X[i];
+            }
+        }
+
+         Console.WriteLine("Масив X");
+        foreach (int num in X)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Мінімальне значення {min}");
+        Console.WriteLine($"Максимальне значення {max}");
+    }
+}
