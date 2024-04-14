0b1af0acb4118779a13ed5742a4e1fb21d859691
WIN-PPAPV0NV3K2\Administrator <walfo2018@gmail.com>
2024-04-14 15:42:24 +0300

    4done

---
+++ pr_2_task_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int studentNumber = 3;
+        int arrayLength = 10 + (studentNumber % 10);
+        int[] X = new int[arrayLength];
+        Random rnd = new Random();
+
+        for (int i = 0; i < X.Length; i++)
+        {
+            X[i] = rnd.Next(-50, 50);
+        }
+
+        Console.Write("Введіть число М ");
+        int M = int.Parse(Console.ReadLine());
+
+        int[] Y = Array.FindAll(X, num => Math.Abs(num) > M);
+
+        Console.WriteLine($"Число M {M}");
+        Console.WriteLine("Масив Х");
+        PrintArray(X);
+        Console.WriteLine("Масив Y");
+        PrintArray(Y);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
